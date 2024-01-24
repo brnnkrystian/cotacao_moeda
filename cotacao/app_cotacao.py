168d18f3afe5611dd5a647d3656fed6967b4cb6c
Brenno Vieira <brennokvieira@gmail.com>
2024-01-23 22:00:03 -0300

    added EUR and BTC

--- cotacao/app_cotacao.py
+++ cotacao/app_cotacao.py
@@ -1,13 +1,29 @@
 import requests
 
 def execute():
-    real = float(input('Insira o valor em R$: '))
-    url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+    moeda_desejada = input('Insira a moeda que deseja: (D) Dolar, (E) Euro, (B) BTC: ')
+    if moeda_desejada == 'D':
+        moeda = 'USD-BRL'
+    elif moeda_desejada == 'E':
+        moeda = 'EUR-BRL'
+    elif moeda_desejada == 'B':
+        moeda = 'BTC-BRL'
+
+    valor_desejado = float(input('Insira o valor em R$: '))
+    url = f'https://economia.awesomeapi.com.br/json/last/{moeda}'
     response = requests.get(url)
     body = response.json()
-    price = float(body['USDBRL']['bid'])
-    valor_convertido = real / price
-    print(f'Cotação do dolar atual: {price:.2f}')
-    print(f'Valor convertido: ${valor_convertido:.2f}')
+    if moeda == 'USD-BRL':
+        coin = 'USDBRL'
+    elif moeda == 'EUR-BRL':
+        coin = 'EURBRL'
+    elif moeda == 'BTC-BRL':
+        coin = 'BTCBRL'
+
+    price = float(body[f'{coin}']['bid'])
+    valor_convertido = valor_desejado / price
+    
+    print(f'Cotação da moeda escolhida: {price:.2f}')
+    print(f'Valor convertido da moeda escolhida: {valor_convertido:.2f}')
 
 execute()
